38248bbcafb52b3ca171f4ca760c4826c3bf9ee2
ariannedee <ariannedee@gmail.com>
2021-01-18 15:16:03 -0800

    Make each question a new view. Improve styling.

--- zoom_trivia/games/urls.py
+++ zoom_trivia/games/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 
-from .views import game_index, round_, start_round
+from .views import game_index, question_view, start_round
 
 app_name = "games"
 urlpatterns = [
     path("<game_id>/", view=game_index, name="game"),
-    path("round/<round_id>/start_round/", view=start_round, name="start_round"),
-    path("round/<round_id>", view=round_, name="round"),
+    path(
+        "<game_id>/round/<round_num>/start_round/", view=start_round, name="start_round"
+    ),
+    path(
+        "<game_id>/round/<round_num>/question/<question_num>",
+        view=question_view,
+        name="question",
+    ),
 ]
--- zoom_trivia/games/views.py
+++ zoom_trivia/games/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 
-from zoom_trivia.games.models import Game, Round
+from zoom_trivia.games.models import Game, Question
 
 
 def game_index(request, game_id):
@@ -9,13 +9,16 @@ def game_index(request, game_id):
     return render(request, "games/index.html", context=context)
 
 
-def start_round(request, round_id):
-    _round = get_object_or_404(Round, pk=round_id)
+def start_round(request, game_id, round_num):
+    game = get_object_or_404(Game, pk=game_id)
+    _round = game.rounds.get(number=round_num)
     context = {"round": _round}
     return render(request, "games/round_start.html", context=context)
 
 
-def round_(request, round_id):
-    _round = get_object_or_404(Round, pk=round_id)
-    context = {"round": _round}
+def question_view(request, game_id, round_num, question_num):
+    game = get_object_or_404(Game, pk=game_id)
+    _round = game.rounds.get(number=round_num)
+    question = get_object_or_404(Question, round=_round, number=question_num)
+    context = {"round": _round, "question": question}
     return render(request, "games/questions.html", context=context)
